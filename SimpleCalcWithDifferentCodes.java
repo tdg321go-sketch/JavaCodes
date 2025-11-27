9e3e929f42fa1093217d98077310f4b249279ec7
TanmayG27 <t.d.g.321go@gmail.com>
2025-11-27 21:02:01 +0530

    Add files via upload

---
+++ SimpleCalcWithDifferentCodes.java
@@ -0,0 +1,84 @@
+
+import java.util.Scanner;
+
+public class SimpleCalcWithDifferentCodes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        // System.out.println("Calculator using If-else & String");
+        // System.out.print("Value A = ");
+        // float A = sc.nextFloat();
+        // System.out.print("Operator = ");
+        // String X = sc.next();
+        // System.out.print("Value B = ");
+        // float B = sc.nextFloat();
+        // if (X.equals("+")) {
+        //     System.out.println("Result = " + ((float)A+B));
+        // }
+        // else if (X.equals("-")) {
+        //     System.out.println("Result = " + ((float)A-B));
+        // }
+        // else if (X.equals("*")){
+        //     System.out.println("Result = " + ((float)A*B));
+        // }
+        // else if (X.equals("/")) {
+        //     System.out.println("Result = " + ((float)A/B));
+        // }
+        // else{
+        //     System.out.println("Invalid Operator");
+        // }
+
+
+        // System.out.println("Calculator using If-else & Char");
+        // System.out.print("Value C = ");
+        // float C = sc.nextFloat();
+        // System.out.print("Operator = ");
+        // char Y = sc.next().charAt(0);
+        // System.out.print("Value D = ");
+        // float D = sc.nextFloat();
+        // if (Y==('+')) {
+        //     System.out.println("Result = " + ((float)C+D));
+        // }
+        // else if (Y==('-')) {
+        //     System.out.println("Result = " + ((float)C-D));
+        // }
+        // else if (Y==('*')){
+        //     System.out.println("Result = " + ((float)C*D));
+        // }
+        // else if (Y==('/')) {
+        //     System.out.println("Result = " + ((float)C/D));
+        // }
+        // else{
+        //     System.out.println("Invalid Operator");
+        // }
+
+
+        // System.out.println("Calculator using Switch & String");
+        // System.out.print("Value A = ");
+        // float A = sc.nextFloat();
+        // System.out.print("Operator = ");
+        // String X = sc.next();
+        // System.out.print("Value B = ");
+        // float B = sc.nextFloat();
+        // switch (X) {
+        //     case "+":
+        //         System.out.println("Result = " + ((float)A+B));  
+        //         break;
+        //     case "-":
+        //         System.out.println("Result = " + ((float)A-B));  
+        //         break;
+        //     case "*":
+        //         System.out.println("Result = " + ((float)A*B));  
+        //         break;
+        //     case "/":
+        //         System.out.println("Result = " + ((float)A/B));
+        //         break;
+        //     default:
+        //     System.out.println("Invalid Operator");
+        // }
+
+
+
+
+    }
+    }
